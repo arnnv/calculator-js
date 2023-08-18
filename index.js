1978322d02da72341d65cac03e82edbf0767284e
Arnav <109935951+arnnv@users.noreply.github.com>
2023-08-18 15:23:22 +0530

    Add event listener to operators

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ function divide(a, b) {
   return a / b;
 }
 
+function modulus(a, b) {
+  return a % b;
+}
+
 let num1;
 let num2;
 let op;
@@ -23,16 +27,30 @@ function operate(num1, op, num2) {
   if (op === "-") return subtract(num1, num2);
   if (op === "*") return multiply(num1, num2);
   if (op === "/") return divide(num1, num2);
+  if (op === "%") return modulus(num1, num2);
 }
 
 const display = document.querySelector("#display-text");
 
 let displayVal = "";
+let operator = "";
+let afterOperator = "";
+
 const numButtons = document.querySelectorAll(".num");
 numButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    let selected = button.textContent;
-    displayVal += selected;
-    display.textContent += selected;
+    if (display.textContent.length <= 12) {
+      let selected = button.textContent;
+      displayVal += selected;
+      display.textContent += selected;
+    }
+  });
+});
+
+const operationButtons = document.querySelectorAll(".op");
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    let operator = button.textContent;
+    console.log(operator);
   });
 });
