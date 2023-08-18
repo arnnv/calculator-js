bca1c92988c44d468208b2921ebb3295c9e41b53
Arnav <109935951+arnnv@users.noreply.github.com>
2023-08-18 11:40:55 +0530

    Create operate function
    Takes operands and operator as input, then calls the appropriate mathematical function

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+let num1;
+let num2;
+let op;
+
+function operate(num1, op, num2) {
+  if (op === "+") return add(num1, num2);
+  if (op === "-") return subtract(num1, num2);
+  if (op === "*") return multiply(num1, num2);
+  if (op === "/") return divide(num1, num2);
+}
